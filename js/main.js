410ae9c043ea6500666d6046f2974dec6a7d2345
True0void <polotor13@gmail.com>
2026-08-01 14:57:38 +0200

    setup the minimal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#vanta-bg", 
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x9469d2,
+  backgroundColor: 0x000000,
+  points: 8.00,
+  maxDistance: 26.00,
+  spacing: 20.00
+});
